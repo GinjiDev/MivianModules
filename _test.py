8d50a84533629e53f07eb8f9d8105c956e588d7f
Гинджи <nerkotin@gmail.com>
2024-07-26 11:59:20 +0300

    miv_sql add

--- _test.py
+++ _test.py
@@ -1,8 +1,15 @@
-from mivmodules import color
+from mivmodules.miv_sql import Database
 import asyncio
 
+db1 = Database("test.db")
 
 async def main():
-    return await color.get_average_rgb_color(url="https://i.pinimg.com/236x/d9/15/61/d9156134f3a7f74ddc1cfc57902d1467.jpg")
+    await db1.create_table('user', ["id INT PRIMARY KEY", "name TEXT DEFAULT NONE"])
+    id_list = [3235, 1435, 24, 32134]
+    
+    for user_id in id_list:
+        rows = await db1.get_where('user', 'id=?', user_id)
+        if not rows:
+            await db1.execute('INSERT INTO user (id) VALUES (?)', user_id)
 
-print(asyncio.run(main()))
+asyncio.run(main())
--- mivmodules/color.py
+++ mivmodules/color.py
@@ -1,15 +1,15 @@
 import aiohttp
 import numpy
-import imageio.v3 as iio
+import imageio
 
 async def get_average_rgb_color(url: str) -> str | None:
-    if not url: return
+    if not url: return (0, 0, 0)
 
     async with aiohttp.ClientSession() as session:
         data = await session.get(url)
         avatar_data = await data.read()
 
-    avatar_img = iio.imread(avatar_data)
+    avatar_img = imageio.imread(avatar_data, format="JPG")
     avatar_img = avatar_img.astype(numpy.uint8)
 
     avg_color = numpy.mean(avatar_img, axis=(0, 1))
